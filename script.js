45af2c4550bebd597f84a4dcd9e242134434c9af
R894 <kamil.khalaileh@gmail.com>
2023-07-06 07:21:56 +0300

    add click&drag and hover options

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var board = document.querySelector(".board");
 var cellCount = 64*64; 
 var cellColor = "blue";
 var hoverToggled = false;
+var mouseDownToggled = false;
+
+var isMouseDown = false;
 
 for (let i = 0; i < cellCount; i++) {
   var cell = document.createElement("div");
@@ -21,26 +24,48 @@ toggleHover();
 
 
 document.getElementById("hover").addEventListener("click", toggleHover);
+document.getElementById("click").addEventListener("click", toggleClick);
 
 function cellColorHandler(){
   this.style.backgroundColor = cellColor;
 }
 
+function handleMouseDown(){
+  isMouseDown = true;
+}
 
+function handleMouseUp(){
+  isMouseDown = false;
+}
 
-function toggleClickAndDrag(bool){
-  if(bool == true){
-    document.querySelectorAll(".cell").forEach(function (cell) {
-      cell.addEventListener("click", cellColorHandler);
-    });
-  }else{
-    document.querySelectorAll(".cell").forEach(function (cell) {
-      cell.removeEventListener("click", cellColorHandler);
-    });
+function HandleMouseMove(e){
+  if(isMouseDown && e.target.classList.contains("cell")){
+    e.target.style.backgroundColor = cellColor;
   }
 }
 
-function toggleHover(bool){
+
+
+
+function toggleClick(){
+  if(!mouseDownToggled){
+    document.querySelectorAll(".cell").forEach(function (cell) {
+      cell.addEventListener("mousedown", handleMouseDown);
+      cell.addEventListener("mouseup", handleMouseUp);
+      cell.addEventListener("mouseover", HandleMouseMove)
+    });
+    mouseDownToggled = true;
+  }else{
+    document.querySelectorAll(".cell").forEach(function (cell) {
+      cell.removeEventListener("mousedown", handleMouseDown);
+      cell.removeEventListener("mouseup", handleMouseUp);
+      cell.removeEventListener("mouseover", HandleMouseMove)
+    });
+    mouseDownToggled = false;
+  }
+}
+
+function toggleHover(){
   if(!hoverToggled){
     document.querySelectorAll(".cell").forEach(function (cell) {
       cell.addEventListener("mouseover", cellColorHandler)
